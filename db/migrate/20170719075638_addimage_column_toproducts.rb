b5d5abb3f7c84a5110a9154b146e70e546277265
Ishwaryaa <user@Ishwaryaa.local>
2017-07-19 16:40:24 +0530

    Delete files - Error

--- db/migrate/20170719075638_addimage_column_toproducts.rb
+++
@@ -1,5 +0,0 @@
-class AddimageColumnToproducts < ActiveRecord::Migration
-   def change
-    add_column :image_url, :string
-  end
-end
--- db/migrate/20170719081052_addimage_url_column_toproduct.rb
+++
@@ -1,7 +0,0 @@
-class AddimageUrlColumnToproduct < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
